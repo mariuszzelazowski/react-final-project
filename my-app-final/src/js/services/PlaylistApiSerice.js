e32c8a665f7e07bccb01cb9cadc936da6a6a4f02
mariuszzelazowski <zelazowskimariusz91@gmail.com>
2022-06-20 16:55:16 +0200

    Small changes

--- my-app-final/src/js/services/PlaylistApiSerice.js
+++ my-app-final/src/js/services/PlaylistApiSerice.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from "react";
 
+const playlistAPI = "https://d3mpvzein3znbf.cloudfront.net/api/playlists/"
+
 const PlaylistApiService = () => {
     const [values, setValues] = useState([]);
 
-    useEffect(() => {
-        fetch("https://d3mpvzein3znbf.cloudfront.net/api/playlists/?format=json&id")
-            .then(response => response.json())
+    useEffect((id) => {
+        fetch(`${playlistAPI}${id}`)
+            .then((response) => response.json())
             .then((data => data.map((el) => (console.log(el)))))
             .catch(err => console.warn(err))
     });
